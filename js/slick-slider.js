06f66b092d928a5c8dcb874e5d59ba2c91277b52
Iryna-Mykhailovska <i.mihaylovskaya05031983@gmail.com>
2026-04-27 20:04:53 +0300

    add section projects and news

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -28,3 +28,31 @@ $(document).ready(function () {
         ]
     });
 });
+$(document).ready(function () {
+    $('.slider_news').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: false,
+                    arrows: true,
+                    autoplaySpeed: 2000,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    autoplaySpeed: 1000,
+                }
+            }
+        ]
+    });
+});
