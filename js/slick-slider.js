07ded1f6659116c6752e0068b00f7bb43beb95c1
Iryna-Mykhailovska <i.mihaylovskaya05031983@gmail.com>
2026-05-05 19:31:05 +0300

    fix

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 3,
-        
+        autoplaySpeed: 2000,
         responsive: [
             {
                 breakpoint: 992,
